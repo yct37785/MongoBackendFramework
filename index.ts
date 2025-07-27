547712f0ae9337971fb9064893d57c9117782fd6
YC-ACER-LAPTOP\yctan <yiechertan@gmail.com>
2025-07-28 00:00:16 +0800

    index.ts exports

--- index.ts
+++ index.ts
@@ -1,6 +1,27 @@
-export * from './src/app';
+/**************************************************************************************************
+ * Entry point for MongoBackendFramework
+ * Exports app modules and dependencies
+ **************************************************************************************************/
 
-// optionally re-export middleware/utilities if you want them public
-// export * from './src/middleware/authMiddleware';
-// export * from './src/error/globalErrorHandler';
-// etc.
+// core framework exports
+export * from './src/controller/authController';
+export * from './src/error/AppError';
+export * from './src/error/globalErrorHandler';
+export * from './src/middleware/asyncHandler';
+export * from './src/middleware/authMiddleware';
+export * from './src/models/userModel';
+export * from './src/services/authServices';
+export * from './src/utils/inputSanitizer';
+export * from './src/utils/misc';
+export { createApp } from './src/app';
+export * from './src/consts';
+
+// common modules re-exported for client usage
+export { default as express } from 'express';
+export { default as mongoose } from 'mongoose';
+export { default as axios } from 'axios';
+export * as uuid from 'uuid';
+
+// express type re-exports
+export type { Request, Response, NextFunction, Router } from 'express';
+export type { ObjectId } from 'mongoose';
