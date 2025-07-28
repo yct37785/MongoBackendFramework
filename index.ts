ad37a892a1071bcf0c9e8801579b3b6a0868f787
yct12321 <yct12321@gmail.com>
2025-07-28 23:37:12 +0800

    move devApp to root and rename to indexSample

--- index.ts
+++ index.ts
@@ -13,6 +13,8 @@ export * from './src/models/userModel';
 export * from './src/services/authServices';
 export * from './src/utils/inputSanitizer';
 export * from './src/utils/misc';
+export * from './src/utils/testUtils';
+export * from './src/utils/setupTestDB';
 export { createApp } from './src/app';
 export * from './src/consts';
 
--- pm2.config.js
+++ pm2.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'mongobackendframework',
-      script: 'dist/src/devApp.js',  // compiled output
+      script: 'dist/indexSample.js', // compiled output
       watch: false,                  // disable file watching in prod
       instances: 1,                  // or "max" for clustering
       autorestart: true,
