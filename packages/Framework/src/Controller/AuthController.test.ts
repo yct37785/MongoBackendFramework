03b1894e3720d21afe4e39a891a57417681d9133
YC-ACER-LAPTOP\yctan <yct37785@gmail.com>
2025-08-31 01:40:12 +0800

    fix uncomment

--- packages/Framework/src/Controller/AuthController.test.ts
+++ packages/Framework/src/Controller/AuthController.test.ts
@@ -32,103 +32,103 @@ describe('con_auth_register', () => {
   });
 });
 
-// /******************************************************************************************************************
-//  * con_auth_login
-//  ******************************************************************************************************************/
-// describe('con_auth_login', () => {
-//   const sameEmail = genTestEmail();
+/******************************************************************************************************************
+ * con_auth_login
+ ******************************************************************************************************************/
+describe('con_auth_login', () => {
+  const sameEmail = genTestEmail();
 
-//   beforeEach(async () => {
-//     await con_auth_register(mockReq({ email: sameEmail, password: TEST_PW }));
-//   });
+  beforeEach(async () => {
+    await con_auth_register(mockReq({ email: sameEmail, password: TEST_PW }));
+  });
 
-//   test('InputError: invalid types', async () => {
-//     await testInvalidInputs(
-//       (email) => con_auth_login(mockReq({ email, password: TEST_PW })),
-//       InputError, invaid_strs);
-//     await testInvalidInputs(
-//       (password) => con_auth_login(mockReq({ email: sameEmail, password })),
-//       InputError, invaid_strs);
-//   });
+  test('InputError: invalid types', async () => {
+    await testInvalidInputs(
+      (email) => con_auth_login(mockReq({ email, password: TEST_PW })),
+      InputError, invaid_strs);
+    await testInvalidInputs(
+      (password) => con_auth_login(mockReq({ email: sameEmail, password })),
+      InputError, invaid_strs);
+  });
 
-//   test('AuthError: wrong credentials', async () => {
-//     await expect(con_auth_login(mockReq({ email: sameEmail, password: 'wrongpw' }))).rejects.toThrow(AuthError);
-//     await expect(con_auth_login(mockReq({ email: 'notfound@example.com', password: TEST_PW }))).rejects.toThrow(AuthError);
-//   });
+  test('AuthError: wrong credentials', async () => {
+    await expect(con_auth_login(mockReq({ email: sameEmail, password: 'wrongpw' }))).rejects.toThrow(AuthError);
+    await expect(con_auth_login(mockReq({ email: 'notfound@example.com', password: TEST_PW }))).rejects.toThrow(AuthError);
+  });
 
-//   test('login successfully', async () => {
-//     const result = await con_auth_login(mockReq({ email: sameEmail, password: TEST_PW }));
-//     expectString(result.accessToken);
-//     expectString(result.refreshToken);
-//     expectDate(result.atExpiresAt);
-//     expectDate(result.rtExpiresAt);
-//   });
-// });
+  test('login successfully', async () => {
+    const result = await con_auth_login(mockReq({ email: sameEmail, password: TEST_PW }));
+    expectString(result.accessToken);
+    expectString(result.refreshToken);
+    expectDate(result.atExpiresAt);
+    expectDate(result.rtExpiresAt);
+  });
+});
 
-// /******************************************************************************************************************
-//  * con_auth_refresh
-//  ******************************************************************************************************************/
-// describe('con_auth_refresh', () => {
-//   const sameEmail = genTestEmail();
-//   let loginData: any;
+/******************************************************************************************************************
+ * con_auth_refresh
+ ******************************************************************************************************************/
+describe('con_auth_refresh', () => {
+  const sameEmail = genTestEmail();
+  let loginData: any;
 
-//   beforeEach(async () => {
-//     await con_auth_register(mockReq({ email: sameEmail, password: TEST_PW }));
-//     loginData = await con_auth_login(mockReq({ email: sameEmail, password: TEST_PW }));
-//   });
+  beforeEach(async () => {
+    await con_auth_register(mockReq({ email: sameEmail, password: TEST_PW }));
+    loginData = await con_auth_login(mockReq({ email: sameEmail, password: TEST_PW }));
+  });
 
-//   test('InputError: invalid refresh token type/format', async () => {
-//     await testInvalidInputs(
-//       (refreshToken) => con_auth_refresh(mockReq({ refreshToken })),
-//       InputError, invaid_strs);
-//   });
+  test('InputError: invalid refresh token type/format', async () => {
+    await testInvalidInputs(
+      (refreshToken) => con_auth_refresh(mockReq({ refreshToken })),
+      InputError, invaid_strs);
+  });
 
-//   test('AuthError: malformed token', async () => {
-//     await expect(con_auth_refresh(mockReq({ refreshToken: malformToken(loginData.refreshToken) }))).rejects.toThrow(AuthError);
-//   });
+  test('AuthError: malformed token', async () => {
+    await expect(con_auth_refresh(mockReq({ refreshToken: malformToken(loginData.refreshToken) }))).rejects.toThrow(AuthError);
+  });
 
-//   test('refresh successfully rotates access token but preserves refresh expiry', async () => {
-//     await wait(2);  // ensure that timestamp based seed will not generate same values
-//     const result = await con_auth_refresh(mockReq({ refreshToken: loginData.refreshToken }));
-//     expectString(result.accessToken);
-//     expectString(result.refreshToken);
-//     expectDate(result.atExpiresAt);
-//     expectDate(result.rtExpiresAt);
-//     expect(result.accessToken).not.toBe(loginData.accessToken);
-//     expect(result.refreshToken).not.toBe(loginData.refreshToken);
-//     expect(result.atExpiresAt.toISOString()).not.toBe(loginData.atExpiresAt.toISOString());   // access token expiry updated
-//     expect(result.rtExpiresAt.toISOString()).toBe(loginData.rtExpiresAt.toISOString());       // refresh token expiry always stay the same
-//     // refresh will fail (refresh token rotated)
-//     await expect(con_auth_refresh(mockReq({ refreshToken: loginData.refreshToken }))).rejects.toThrow(AuthError);
-//   });
-// });
+  test('refresh successfully rotates access token but preserves refresh expiry', async () => {
+    await wait(2);  // ensure that timestamp based seed will not generate same values
+    const result = await con_auth_refresh(mockReq({ refreshToken: loginData.refreshToken }));
+    expectString(result.accessToken);
+    expectString(result.refreshToken);
+    expectDate(result.atExpiresAt);
+    expectDate(result.rtExpiresAt);
+    expect(result.accessToken).not.toBe(loginData.accessToken);
+    expect(result.refreshToken).not.toBe(loginData.refreshToken);
+    expect(result.atExpiresAt.toISOString()).not.toBe(loginData.atExpiresAt.toISOString());   // access token expiry updated
+    expect(result.rtExpiresAt.toISOString()).toBe(loginData.rtExpiresAt.toISOString());       // refresh token expiry always stay the same
+    // refresh will fail (refresh token rotated)
+    await expect(con_auth_refresh(mockReq({ refreshToken: loginData.refreshToken }))).rejects.toThrow(AuthError);
+  });
+});
 
-// /******************************************************************************************************************
-//  * con_auth_logout
-//  ******************************************************************************************************************/
-// describe('con_auth_logout', () => {
-//   const sameEmail = genTestEmail();
-//   let loginData: any;
+/******************************************************************************************************************
+ * con_auth_logout
+ ******************************************************************************************************************/
+describe('con_auth_logout', () => {
+  const sameEmail = genTestEmail();
+  let loginData: any;
 
-//   beforeEach(async () => {
-//     await con_auth_register(mockReq({ email: sameEmail, password: TEST_PW }));
-//     loginData = await con_auth_login(mockReq({ email: sameEmail, password: TEST_PW }));
-//   });
+  beforeEach(async () => {
+    await con_auth_register(mockReq({ email: sameEmail, password: TEST_PW }));
+    loginData = await con_auth_login(mockReq({ email: sameEmail, password: TEST_PW }));
+  });
 
-//   test('InputError: invalid refresh token type/format', async () => {
-//     await testInvalidInputs(
-//       (refreshToken) => con_auth_logout(mockReq({ refreshToken })),
-//       InputError, invaid_strs);
-//   });
+  test('InputError: invalid refresh token type/format', async () => {
+    await testInvalidInputs(
+      (refreshToken) => con_auth_logout(mockReq({ refreshToken })),
+      InputError, invaid_strs);
+  });
 
-//   test('AuthError: malformed token', async () => {
-//     await expect(con_auth_logout(mockReq({ refreshToken: malformToken(loginData.refreshToken) }))).rejects.toThrow(AuthError);
-//   });
+  test('AuthError: malformed token', async () => {
+    await expect(con_auth_logout(mockReq({ refreshToken: malformToken(loginData.refreshToken) }))).rejects.toThrow(AuthError);
+  });
 
-//   test('logout successfully invalidates session', async () => {
-//     const result = await con_auth_logout(mockReq({ refreshToken: loginData.refreshToken }));
-//     expect(result.msg).toBe('Logged out of session');
-//     // refresh will fail (session removed)
-//     await expect(con_auth_refresh(mockReq({ refreshToken: loginData.refreshToken }))).rejects.toThrow(AuthError);
-//   });
-// });
+  test('logout successfully invalidates session', async () => {
+    const result = await con_auth_logout(mockReq({ refreshToken: loginData.refreshToken }));
+    expect(result.msg).toBe('Logged out of session');
+    // refresh will fail (session removed)
+    await expect(con_auth_refresh(mockReq({ refreshToken: loginData.refreshToken }))).rejects.toThrow(AuthError);
+  });
+});
